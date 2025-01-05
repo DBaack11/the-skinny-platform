26ecb9daaef113c2fb9803b98eae35cf221ad272
DBaack11 <dfbaack@gmail.com>
2025-01-04 19:44:05 -0600

    Updated home page layout and styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'system-ui', 'sans-serif'],
+      'lie': ['Lie To Me', 'cursive'],
+    },
     extend: {
       colors: {
         "skinny-blue": "#6A8CAF",
